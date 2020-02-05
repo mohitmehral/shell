6260e08faad2501343e7c3057d7fa030f4d5df98
mohitmehral <mohitmehral@gmail.com>
2020-02-05 16:29:23 +0100

    cron added
    29 * * * * /bin/sh /root/pingLog.shi   # cron created to secure the VPN tunnel. ping in every 29 min to activate the tunnel

--- pingLog.sh
+++ pingLog.sh
@@ -1,5 +1,10 @@
 #!/bin/bashi
 # ping script which log the packets analysis in log file
+# Run every 29 min  
+# 29 * * * * /bin/sh /root/pingLog.shi   # cron created to secure the VPN tunnel. ping in every 29 min to activate the tunnel
+~
+~
+
 # Mohit Mehral
 
 log_file=`date +%Y%m%d_ping.log`
